ad449f3a055d6126d6febdc681ef8b9b75915b63
Ger Hobbelt <ger@hobbelt.com>
2020-06-27 17:26:12 +0200

    lint fixes

--- benchmark/profile.js
+++ benchmark/profile.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
-/*eslint no-console:0*/
+/* eslint no-console:0 no-bitwise: 0 */
 
 
 let fs = require('fs');
 let path = require('path');
-var Benchmark = require('benchmark');
-var microtime = require('microtime');
+let Benchmark = require('benchmark');
+let microtime = require('microtime');
 
 // see if user specifieed a number of seconds to run:
 let arg = process.argv[process.argv.length - 1];
@@ -28,13 +28,13 @@ let data = fs.readFileSync(path.join(__dirname, '../test/fixtures/commonmark/spe
 
 let tdelta;
 do {
-for (let i = 0; i < 20; i++) {
-  output = md.render(data);
-}
+  for (let i = 0; i < 20; i++) {
+    output = md.render(data);
+  }
 
-let t1 = microtime.nowDouble();
+  let t1 = microtime.nowDouble();
 
-tdelta = t1 - t0;
+  tdelta = t1 - t0;
 } while (tdelta < maxSeconds);
 
 let tsec = tdelta | 0;
@@ -43,7 +43,7 @@ let tmsec = tdelta | 0;
 tdelta = (tdelta - tmsec) * 1000;
 let tusec = Math.round(tdelta);
 
-  console.log(`Total time taken for the run is ${tsec} seconds, ${tmsec} ms and ${tusec} us.`);
+console.log(`Total time taken for the run is ${tsec} seconds, ${tmsec} ms and ${tusec} us.`);
 
 console.log('Test Finished.');
 
